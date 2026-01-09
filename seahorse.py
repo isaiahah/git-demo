e0c0924fb6c673625a9c0c03439dbe59386a1a68
Isaiah Hazelwood <isaiah.hazelwood@ucsf.edu>
2026-01-08 16:24:44 -0800

    Answer angry questions about Mayotte

--- seahorse.py
+++ seahorse.py
@@ -2,3 +2,5 @@
 
 print("Q: Is there a seahorse emoji?")
 print("A: Yes. It's 🐟")
+print("Q: 🤓🤓 There's a seahorse in the flag of Mayotte 🇾🇹")
+print("A: Well that's not a seahorse that's a flag showing a seahorse. Ceci n'est pas un cheval du mer")
